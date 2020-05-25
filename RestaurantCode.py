c3fc93f0a723f5f857ae10d0d6000d0c4eed5cb1
BrentV23 <65264136+BrentV23@users.noreply.github.com>
2020-05-26 09:20:23 +1000

    Update RestaurantCode.py

--- RestaurantCode.py
+++ RestaurantCode.py
@@ -1,23 +1,47 @@
 import pandas as pd
 
-df = pd.read_csv("https://raw.githubusercontent.com/BrentV23/Brent-V-Data-Science/master/Restaurant_Data.csv")
+df = pd.read_csv("https://raw.githubusercontent.com/BrentV23/Brent-V-Data-Science/master/Restaurant_Data_New.csv")
 
-def filter():
-  cuisine_valid = False
-  while cuisine_valid == False:
-    cuisine = input("Enter Cuisine: ")
-    if cuisine == "Western" or cuisine == "Asian":
-      cuisine_valid = True
+cuisineValid = False
+while cuisineValid == False:
+  cuisine = input("Enter cuisine: ").lower()
+  if cuisine == "western":
+    cuisineValid = True
+    cuisine = "Western"
+  elif cuisine == "asian":
+    cuisineValid = True
+    cuisine = "Asian"
+    
+deliveryValid = False
+while deliveryValid == False:
+  delivery = input("Delivery?: ").lower()
+  if delivery == "yes" or delivery == "y":
+    deliveryValid = True
+    delivery = "Yes"
+  elif delivery == "no" or delivery == "n":
+    deliveryValid = True
+    delivery = "No"
   
-  delivery_valid = False
-  while delivery_valid == False:
-    delivery = input("Delivery?: ")
-    if delivery == "Yes" or delivery == "yes" or delivery == "Y" or delivery == "y":
-      delivery_valid = True
 
-  location_valid = False
-  while location_valid == False:
-    location = input("Enter Location: ")
-    if location == "Belconnen" or location == "CBD" or location == "Woden" or location == "Gungahlin":
-      location_valid = True
-filter()
+locationValid = False
+while locationValid == False:
+  location = input("Enter location: ").lower()
+  if location == "belconnen":
+    locationValid = True
+    location = "Belconnen"
+  elif location == "cbd":
+    locationValid = True
+    location = "CBD"
+  elif location == "woden":
+    locationValid = True
+    location = "Woden"
+  elif location == "gungahlin":
+    locationValid = True
+    location = "Gungahlin"
+
+if delivery == "Yes":
+  df = df[df.Delivery == delivery]
+
+df = df[df.Cuisine == cuisine]
+df = df[df.Average_rating >= 4]
+df[df.Location == location]
